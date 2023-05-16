b631fa82a0ccee66b335cc83f2f650feff944c38
gitytd <demiryahyatayyip@gmail.com>
2023-05-16 18:57:49 +0300

    Initial Commit

---
+++ i_am_poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i_am_poor
+//
+//  Created by Yahya Demir on 16.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
